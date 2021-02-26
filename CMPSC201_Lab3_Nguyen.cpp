979416c190e2e065926ee0129c163a8ea9100a20
Hoang Nguyen <77690906+HoangNguyen55@users.noreply.github.com>
2021-02-25 19:09:55 -0500

    change variable name

--- CMPSC201_Lab3_Nguyen.cpp
+++ CMPSC201_Lab3_Nguyen.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 double N_shares, P_shares, P_commission, A_return, Year;
-double totalPaidNoComs, totalComsFee, totalPaid, totalTenYears;
+double totalPaidNoComs, totalComsFee, totalPaid, totalYears;
 char input;
 
 int main()
@@ -29,8 +29,8 @@ int main()
     cout << "The amount of the commission fee: " << totalComsFee << "\n";
     totalPaid = totalComsFee + totalPaidNoComs;
     cout << "The total amount you paid(Coms fee incl): " << totalComsFee + totalPaidNoComs << "\n";
-    totalTenYears = totalPaidNoComs * pow(1 + A_return/100, Year);
-    cout << "After 10 years, all of your shares will worth: " << totalTenYears << "\n";
+    totalYears = totalPaidNoComs * pow(1 + A_return/100, Year);
+    cout << "After 10 years, all of your shares will worth: " << totalYears << "\n";
     
     cout << "Would you like a receipt? Y/N\n";
     cin >> input;
@@ -44,7 +44,7 @@ int main()
         output << setw(30) << left << "Commission: " << right << setw(1) << "$" << setw(10) << totalComsFee << "\n";
         output << setw(30) << left << "Total amount: " << right << setw(1) << "$" << setw(10) << totalPaid << "\n";
         string str = "Net worth in " + to_string((int)Year) + " years: ";
-        output << setw(30) << left << str << right << setw(1) << "$" << setprecision(2) << setw(10) << totalTenYears << "\n";
+        output << setw(30) << left << str << right << setw(1) << "$" << setprecision(2) << setw(10) << totalYears << "\n";
         cout << "Receipt printed";
         output.close();
     } 
